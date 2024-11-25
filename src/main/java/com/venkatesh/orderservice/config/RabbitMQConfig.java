fb6dc91e1bb9a33db18e996bb13ab36d662ca6c4
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-11-25 14:01:32 -0500

    skeleton added for RabbitMQConfig

---
+++ src/main/java/com/venkatesh/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,13 @@
+package com.venkatesh.orderservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    // spring bean for queue - order queue
+    // spring bean for exchange
+    // spring bean for binding exchange and queue using routing key
+    // message converter
+    // configure RabbitTemplate
+}
